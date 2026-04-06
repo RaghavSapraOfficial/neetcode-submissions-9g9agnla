79299f28f8ab9de096c9bdd33d547eedd02f6ff6
Raghav Sapra <148759867+RaghavSapraOfficial@users.noreply.github.com>
2026-04-06 09:55:37 +0530

    Bulk sync: 2 submissions

---
+++ Data Structures & Algorithms/concatenation-of-array/submission-0.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> getConcatenation(vector<int>& nums) {
+        vector<int>res(nums.begin(),nums.end());
+        for(auto& i: nums){
+            res.push_back(i);
+        }
+        return res;
+    }
+};
